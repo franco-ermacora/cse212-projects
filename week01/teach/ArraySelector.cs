6b028a0fec6526b3f4b301e1a33f5d9bf93d67ad
Franco Ermacora <ermacora.fran@gmail.com>
2025-07-07 23:50:45 -0300

    W01 Individual Activity: Dynamic Arrays and Performance Analysis

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,24 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        int[] result = new int[select.Length];
+        int counter1 = 0;
+        int counter2 = 0;
+
+        for (int i = 0; i < select.Length; i++)
+    {
+        if (select[i] == 1)
+        {
+            result[i] = list1[counter1];
+            counter1++;
+        }
+        else if (select[i] == 2)
+        {
+            result[i] = list2[counter2];
+            counter2++;
+        }
+    }
+
+        return result;
     }
 }
